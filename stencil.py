15436284d69d05aeba9800976e726c7e19970614
Mehryar Sadati <mehryarsadati@gmail.com>
2025-07-20 19:19:35 +0330

    main code added

---
+++ stencil.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageFilter, ImageOps
+import numpy as np
+import cv2
+
+image_path = "photo.jpg"
+image = Image.open(image_path)
+
+gray_image = ImageOps.grayscale(image)
+
+image_np = np.array(gray_image)
+
+blurred = cv2.GaussianBlur(image_np, (5, 5), 0)
+
+edges = cv2.Canny(blurred, threshold1=30, threshold2=100)
+edges_image = Image.fromarray(edges)
+
+stencil_image = ImageOps.invert(edges_image)
+
+stencil_image.save("photo_stencil.png")
+stencil_image.show()
